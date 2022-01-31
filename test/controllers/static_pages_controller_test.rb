56487274cd357c7099b1caaad17a9c31fec1ccc4
Syed Sohail-Nuware <syed.sohail@nuware.com>
2022-01-31 22:39:24 +0530

    dynamic page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,26 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @msg = 'Ruby on Rails Tutorial Sample App'    
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial
-Sample App"
+    assert_select "title", "Home | #{@msg}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial
-Sample App"
-  end
+    assert_select "title", "Help | #{@msg}"
+    end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial
-Sample App"
+    assert_select "title", "About | #{@msg}"
     end
 end
