1550ecc47254ca2e534692ef7590f41f05161ac7
Syed Sohail-Nuware <syed.sohail@nuware.com>
2022-02-07 19:53:25 +0530

    User Validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'static_pages#home'
   get '/help',to: 'static_pages#help', as: 'help'
   get '/about',to: 'static_pages#about', as: 'about'
   get '/contacts',to: 'static_pages#contacts', as:'contacts'
+  get '/signup', to: 'users#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   # root "application#hello"
 end
+ 
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,29 +7,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
   
   test "should get home" do
-    # get root_path
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Home | #{@msg}"
   end
 
   test "should get help" do
     get help_path
-    get static_pages_help_url
     assert_response :success
     assert_select "title", "Help | #{@msg}"
     end
 
   test "should get about" do
     get about_path
-    get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@msg}"
     end
 
   test "should get contacts" do
     get contacts_path
-    get static_pages_contacts_url
     assert_response :success
     assert_select "title", "Contact | #{@msg}"
     end
